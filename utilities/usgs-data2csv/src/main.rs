7979c2ddf7c765cb03911b061c6e2a6ec9ead4a8
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-08-15 16:36:59 -0400

    Run carrgo fmt; minor changes

--- utilities/usgs-data2csv/src/main.rs
+++ utilities/usgs-data2csv/src/main.rs
@@ -4,7 +4,7 @@ use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
 #[derive(Parser)]
-#[clap(about = "Converts the fixed length data from usgs website to csv format")]
+#[clap(about = "Converts the tab sep data from usgs website to csv format.")]
 struct Cli {
     /// Comment Character
     #[clap(short, long, default_value = "#")]
@@ -15,7 +15,7 @@ struct Cli {
     /// Echo Contents (Skips comments)
     #[clap(short, long)]
     echo: bool,
-    /// Output file
+    /// TODO Output file
     #[clap(short, long)]
     output: Option<PathBuf>,
     /// Number of head lines to print
@@ -24,7 +24,7 @@ struct Cli {
     /// Specific line numbers to skip (Comments not counted)
     #[clap(short, long)]
     skip: Vec<u64>,
-    /// Recursively parse directories, otherwise skip directories
+    /// TODO Recursively parse directories, otherwise skip directories
     #[clap(short, long)]
     recursive: bool,
     /// File list to be parsed
@@ -32,38 +32,40 @@ struct Cli {
     files: Vec<PathBuf>,
 }
 
-
 fn main() {
-    let args:Cli = Cli::parse();
-    let mut file:File;
+    let args: Cli = Cli::parse();
+    let mut file: File;
     let mut reader;
     let mut count = 0u64;
     let mut skipped = 0u64;
-    
-    for filename in args.files {
-	file = File::open(&filename).unwrap();
-	reader = BufReader::new(file);
-	
-	for line in reader.lines() {
-            let line = line.unwrap();
-	    if !line.starts_with(args.comment_chars){
-		count += 1;
-		if args.skip.contains(&count) {
-		    skipped += 1;
-		    continue;
-		}
-		
-		if args.echo {
-		    println!("{}", line);
-		} else {
-		    let row:Vec<&str> = line.split("\t").map(|x| x.trim()).collect();
-		    println!("{}", row.join(&args.delineate.to_string()))
-		}
 
-		if Some(count - skipped) == args.head {
-		    break;
-		}
-	    }
-    }
+    for filename in args.files {
+        if !filename.is_file() {
+            continue;
+        }
+        file = File::open(&filename).unwrap();
+        reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if !line.starts_with(args.comment_chars) {
+                count += 1;
+                if args.skip.contains(&count) {
+                    skipped += 1;
+                    continue;
+                }
+
+                if args.echo {
+                    println!("{}", line);
+                } else {
+                    let row: Vec<&str> = line.split("\t").map(|x| x.trim()).collect();
+                    println!("{}", row.join(&args.delineate.to_string()))
+                }
+
+                if Some(count - skipped) == args.head {
+                    break;
+                }
+            }
+        }
     }
 }
