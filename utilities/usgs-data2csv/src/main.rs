acbfc93cf68c91947e5b4c4951eaa0047e21d13f
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-09-16 13:39:50 -0400

    Sanitize cells with comma

--- utilities/usgs-data2csv/src/main.rs
+++ utilities/usgs-data2csv/src/main.rs
@@ -15,6 +15,9 @@ struct Cli {
     /// Echo Contents (Skips comments)
     #[clap(short, long)]
     echo: bool,
+    /// Ignore Errors
+    #[clap(short, long)]
+    ignore_errors: bool,
     /// TODO Output file
     #[clap(short, long)]
     output: Option<PathBuf>,
@@ -32,12 +35,22 @@ struct Cli {
     files: Vec<PathBuf>,
 }
 
+fn sanitize_cell(raw:&str) -> String {
+    let cell = raw.trim();
+    if cell.contains(",") {
+	return format!("\"{}\"", cell);
+    } else{
+	return cell.to_string();
+    }
+}
+
 fn main() {
     let args: Cli = Cli::parse();
     let mut file: File;
     let mut reader;
     let mut count = 0u64;
     let mut skipped = 0u64;
+    let mut col_len:Option<usize> = None;
 
     for filename in args.files {
         if !filename.is_file() {
@@ -46,7 +59,7 @@ fn main() {
         file = File::open(&filename).unwrap();
         reader = BufReader::new(file);
 
-        for line in reader.lines() {
+        for (i, line) in reader.lines().enumerate() {
             let line = line.unwrap();
             if !line.starts_with(args.comment_chars) {
                 count += 1;
@@ -58,7 +71,17 @@ fn main() {
                 if args.echo {
                     println!("{}", line);
                 } else {
-                    let row: Vec<&str> = line.split("\t").map(|x| x.trim()).collect();
+                    let row: Vec<String> = line.split("\t").map(sanitize_cell).collect();
+		    if col_len.is_none(){
+			col_len = Some(row.len());
+		    } else if !(Some(row.len()) == col_len){
+			if args.ignore_errors {
+			    eprintln!("Length Not matched on Line {} (original: {})", count, i);
+			    continue;
+			}else{
+			    panic!("Length Not matched");
+			}
+		    }
                     println!("{}", row.join(&args.delineate.to_string()))
                 }
 
